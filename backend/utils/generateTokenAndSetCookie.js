69f4db69761e0c86558457bf77bd5ea74cbc46ed
ravijat2026 <ravijat2026@gmail.com>
2025-02-05 08:35:50 +0530

    modified

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -8,8 +8,8 @@ const generateTokenAndSetCookie = (res,userId) => {
 
   res.cookie("token",token,{
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: "None",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 *1000,
   });
 
